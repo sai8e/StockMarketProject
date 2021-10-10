57a530bf04c236e4b295646a31e45bd9c7da8ebd
JNope <91224053+JNope01@users.noreply.github.com>
2021-10-10 16:31:08 -0600

    Update By Jon

--- Main/RAW_CSV_Mod.py
+++ Main/RAW_CSV_Mod.py
@@ -9,34 +9,29 @@ main_path = os.path.dirname(__file__)             ## get the current working dir
 raw_path = main_path+'/RAW_STOCK_DATA/'      ## add path to cwd for raw csv file path
 ex_location = main_path + '/ML_STOCK_DATA/'   ## add path to cwd for export of data csv files
 
-os.chdir(raw_path)
-file_extension = ".csv"
-all_filenames = [i for i in glob.glob(f"*{file_extension}")]
+os.chdir(raw_path)                            ## changes cwd raw_path
+file_extension = ".csv"                       ## sets desired file_extension
+all_filenames = [i for i in glob.glob(f"*{file_extension}")] ## gets all files with the set file_extention
 
-for file in all_filenames:
-   ex_filename = file[:file.find('_')]+"_READY.csv"
+for file in all_filenames:                           ## parses through desired files
+   ex_filename = file[:file.find('_')]+"_READY.csv"   ## sets export filename
 
-   os.chdir(main_path)
-   os.chdir(ex_location)
-   with open(ex_filename, 'w', newline='\n') as newCSV:
-       fieldnames = ['Date', 'OpenValue', 'CloseValue', 'DayChange%']
-       write = csv.DictWriter(newCSV, fieldnames=fieldnames)
-       write.writeheader()
-       os.chdir(main_path)
-       os.chdir(raw_path)
-       with open(file, 'r', newline='\n') as rawCSV:
-           rows_read = csv.reader(rawCSV, delimiter=',')
-           header = next(rows_read)
-           if header != None:
-               for row in rows_read:
-                   index = '$'
-                   date = row[0]
-                   openVal = float(row[3].replace(index, ''))
-                   closeVal = float(row[1].replace(index, ''))
-                   dayChangeP = (closeVal-openVal)/closeVal*100
-                   print(dayChangeP)
-                   os.chdir(main_path)
-                   os.chdir(ex_location)
-                   write.writerow({'Date': date, 'OpenValue': openVal, 'CloseValue': closeVal, 'DayChange%': dayChangeP})
-                   os.chdir(main_path)
-                   os.chdir(raw_path)
+   os.chdir(ex_location)                              ## change cwd to export location
+   with open(ex_filename, 'w', newline='\n') as newCSV: ## creates export csv file
+       fieldnames = ['Date', 'OpenValue', 'CloseValue', 'DayChange%']  ## sets header names
+       write = csv.DictWriter(newCSV, fieldnames=fieldnames)  ## creates formats writer
+       write.writeheader()                                    ## writes header
+       os.chdir(raw_path)                                     ## change cwd the raw file location
+       with open(file, 'r', newline='\n') as rawCSV:          ## opens csv file
+           rows_read = csv.reader(rawCSV, delimiter=',')      ## set up the row reader
+           header = next(rows_read)                           ## sets first row to header
+           if header != None:                                 ## if row is not the header
+               for row in rows_read:                          ## reads each row
+                   index = '$'                                ## sets value index
+                   date = row[0]                              ## sets date value
+                   openVal = float(row[3].replace(index, '')) ## sets openVal value to flaot anf removes value index
+                   closeVal = float(row[1].replace(index, '')) ## sets closeVal value to flaot anf removes value index
+                   dayChangeP = (closeVal-openVal)/closeVal*100 ## compares openVal to closeVal and get percentage
+                   os.chdir(ex_location)                      ## change cwd to export location
+                   write.writerow({'Date': date, 'OpenValue': openVal, 'CloseValue': closeVal, 'DayChange%': dayChangeP}) ## writes values to export File
+
