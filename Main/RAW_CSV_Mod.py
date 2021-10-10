bd9de2139273bbca0cf84ad1fcd3d16dcecb9a15
JNope <91224053+JNope01@users.noreply.github.com>
2021-10-10 12:34:35 -0600

    Update By Jon

--- Main/RAW_CSV_Mod.py
+++ Main/RAW_CSV_Mod.py
@@ -1,7 +1,12 @@
 ## This program takes the Raw Stock DATA and Modifies it, it can be added to the Machine Learning Algorium
 
+import os
 import csv
 
-with open("..\\RAW_STOCK_DATA\\AAPL_historical_data.csv") as rawCSV:
+current_path = os.path.dirname(__file__)
+raw_path = os.path.relpath("..\\RAW_STOCK_DATA\\AAPL_historical_data.csv", current_path)
+export_location = os.path.relpath("..\\ML_STOCK_DATA\\AAPL_historical_data.csv", current_path)
+
+with open(raw_path) as rawCSV:
     csv_read = csv.reader(rawCSV, delimiter=',')
-print(csv_read)
+
