a92c13aa400d98a014ff5dd72b8224c9877350c7
JNope <91224053+JNope01@users.noreply.github.com>
2021-10-13 14:48:50 -0600

    Update By Jon

--- Main/Bulk Mess/RAW_CSV_Mod.py
+++ Main/Bulk Mess/RAW_CSV_Mod.py
@@ -5,16 +5,34 @@
 import os
 import csv
 import glob
+import shutil
 
 
 main_path = os.path.dirname(__file__)                                                                                      ## get the current working directory(cwd) of the RAW_CSV_Mod.py file
 raw_path = main_path+'/RAW_STOCK_DATA/'                                                                                    ## add path to cwd for raw csv file path
 ex_location = main_path + '/ML_STOCK_DATA/'                                                                                ## add path to cwd for export of data csv files
+flip_location = main_path + '/FL_CSVs/'
 
 os.chdir(raw_path)                                                                                                         ## changes cwd raw_path
 prevClose = float()
 file_extension = ".csv"                                                                                                    ## sets desired file_extension
 all_filenames = [i for i in glob.glob(f"*{file_extension}")]                                                               ## gets all files with the set file_extention
+
+for file in all_filenames:
+    fliped_filename = file[:file.find('_')] + "_Flipped.csv"
+    with open(file) as rawCSV, open(fliped_filename, 'w', newline='\n') as flippedCSV:
+        rows_read = csv.reader(rawCSV, delimiter=',')  ## set up the row reader
+        header = next(rows_read)  ## sets first row to header
+        if header != None:
+            cw = csv.writer(flippedCSV, delimiter=",")
+
+shutil.move(file, flip_location+fliped_filename)
+
+
+os.chdir(flip_location)  ## changes cwd flip_location
+prevClose = float()
+file_extension = ".csv"  ## sets desired file_extension
+all_filenames = [i for i in glob.glob(f"*{file_extension}")]  ## gets all files with the set file_extention
 
 for file in all_filenames:                                                                                                 ## parses through desired files
    ex_filename = file[:file.find('_')]+"_READY.csv"                                                                        ## sets export filename
@@ -24,7 +42,7 @@ for file in all_filenames:                                                      
        fieldnames = ['Date', 'OpenValue', 'CloseValue', 'DayChange%', 'OpenChange%']                                       ## sets header names
        write = csv.DictWriter(newCSV, fieldnames=fieldnames)                                                               ## creates formats writer
        write.writeheader()                                                                                                 ## writes header
-       os.chdir(raw_path)                                                                                                  ## change cwd the raw file location
+       os.chdir(flip_location)                                                                                                  ## change cwd the raw file location
        with open(file, 'r', newline='\n') as rawCSV:                                                                       ## opens csv file
            rows_read = csv.reader(rawCSV, delimiter=',')                                                                   ## set up the row reader
            header = next(rows_read)                                                                                        ## sets first row to header
