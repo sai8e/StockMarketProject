70c1bc396783d297c31b9e3407c34623805f1133
JNope <91224053+JNope01@users.noreply.github.com>
2021-10-25 15:05:33 -0600

    Update By Jon

--- Main/Stock_Data_Getter.py
+++ Main/Stock_Data_Getter.py
@@ -1,5 +1,4 @@
-## Written By Jonathan Nope ##
-
+# Written By Jonathan Nope #
 
 
 import pandas_datareader as pdr
@@ -9,46 +8,36 @@ import csv
 import datetime as dt
 import glob
 
-
-
 main_path = os.path.dirname(__file__)
-Export_Folder = main_path+"/Stock Data/"
+Export_Folder = main_path + "/Stock Data/"
 index_file = "INDEX.csv"
 index = []
 date = dt.datetime.now()
 end = date.strftime('%Y') + '-' + date.strftime('%m') + '-' + date.strftime('%d')
-start = str(int(date.strftime('%Y'))-2) + '-' + date.strftime('%m') + '-' + date.strftime('%d')
+start = str(int(date.strftime('%Y')) - 2) + '-' + date.strftime('%m') + '-' + date.strftime('%d')
 yf.pdr_override()
 file_extension = ".csv"
-
-
 
 with open(index_file, 'r', newline='\n') as INDEX_CSV:
     INDEX_read = csv.reader(INDEX_CSV, delimiter=',')
     header = next(INDEX_read)
-    if header != None:
+    if header is not None:
         for row in INDEX_read:
             index.append(row[0])
-
-
 
 os.chdir(Export_Folder)
 all_filenames = [i for i in glob.glob(f"*{file_extension}")]
 for file in all_filenames:
     os.remove(file)
 
-
-
 for ticker in index:
     data = pdr.get_data_yahoo(ticker, start, end)
-    data.to_csv(ticker+'.csv')
-
-
+    data.to_csv(ticker + '.csv')
 
 all_filenames = [i for i in glob.glob(f"*{file_extension}")]
 for file in all_filenames:
     previousAVG = 0
-    ex_filename = file[:file.find('.')]+"_READY.csv"
+    ex_filename = file[:file.find('.')] + "_READY.csv"
     with open(ex_filename, 'w', newline='\n') as newCSV:
         fieldnames = ['Date', 'OpenValue', 'CloseValue', 'DayAverage', 'DayAVGChange']
         write = csv.DictWriter(newCSV, fieldnames=fieldnames)
@@ -56,17 +45,16 @@ for file in all_filenames:
         with open(file, 'r', newline='\n') as rawCSV:
             rows_read = csv.reader(rawCSV, delimiter=',')
             header = next(rows_read)
-            if header != None:
+            if header is not None:
                 for row in rows_read:
                     sym = '$'
                     date = row[0]
                     openVal = float(row[3].replace(sym, ''))
                     closeVal = float(row[4].replace(sym, ''))
-                    dayAVG = (openVal+closeVal)/2
-                    dayAVGChange = (dayAVG-previousAVG)
+                    dayAVG = (openVal + closeVal) / 2
+                    dayAVGChange = (dayAVG - previousAVG)
                     write.writerow({'Date': date, 'OpenValue': openVal, 'CloseValue': closeVal, 'DayAverage': dayAVG,
                                     'DayAVGChange': dayAVGChange})
                     prevClose = float(row[1].replace(sym, ''))
                     previousAVG = dayAVG
     os.remove(file)
-
